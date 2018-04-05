de8f007a0ceb8c51cbee8aec55fc961887ef893b
DevGracyy <dev.gracyy.002@gmail.com>
2018-04-05 17:58:34 +0800

    added show password(hide and show)

--- login/login.php
+++ login/login.php
@@ -1,62 +1,64 @@
 <html>
-	<head>
+<head>
 
-	</head>
-	<body>
-		<div class="col-md-4 col-sm-4 animated fadeInRight">
+</head>
+<body>
+	<div class="col-md-4 col-sm-4 animated fadeInRight">
 		<div class="panel panel-default well" id="reportpanel" >
 			<div class="report-form">
-			<div class="container">
-				<form method="GET">
-					<h4 class="form-signin-heading">Get started!</h4>Make a new report, tell us your story or hear from us. <p></p>
+				<div class="container">
+					<form method="GET">
+						<h4 class="form-signin-heading">Get started!</h4>Make a new report, tell us your story or hear from us. <p></p>
 
-					<div class="row col-sm-12" id="hbtnrow">
-          </div>
+						<div class="row col-sm-12" id="hbtnrow">
+						</div>
 
 
-				</form>
+					</form>
+				</div>
 			</div>
-		  </div>
 		</div>
-		</div>
+	</div>
 
-		<div class="col-md-4 col-sm-4 animated fadeInRight">
+	<div class="col-md-4 col-sm-4 animated fadeInRight">
 
 		<div class="panel panel-default well" id="loginpanel" >
 
 
 
 
-		  <div class="signin-form">
-			<div class="container ">
-				<div id="login-area">
-				<?php if ($_SESSION['loggedIn']) {
-						require_once ("login/logout.php");
-				} else{ ?>
-				<form class="form-signin" id='login-form' method="POST">
-				<p><h4 class="form-signin-heading">Update your report,</h4>check its status and talk with us.</p>
-					 <div id="error">
-					<!-- error will be shown here ! -->
-					</div>
+			<div class="signin-form">
+				<div class="container ">
+					<div id="login-area">
+						<?php if ($_SESSION['loggedIn']) {
+							require_once ("login/logout.php");
+						} else{ ?>
+							<form class="form-signin" id='login-form' method="POST">
+								<p><h4 class="form-signin-heading">Update your report,</h4>check its status and talk with us.</p>
+								<div id="error">
+									<!-- error will be shown here ! -->
+								</div>
 
-					<div class="form-group">
-						<input type="text" class="form-control" id="user_email"  id="username_input" name="user_email" placeholder="Reference ID" autofocus="" />
-					</div>
+								<div class="form-group">
+									<input type="text" class="form-control" id="user_email"  id="username_input" name="user_email" placeholder="Reference ID" autofocus="" />
+								</div>
 
-					<div class="form-group">
-						<input type="password" class="form-control" id="password"  name="password" placeholder="Password" />
-					</div>
+								<div class="form-group">
+									<input type="password" class="form-control" id="password"  name="password" placeholder="Password" />
+									<i id="closeeye" style="float: right;margin-left: -23px;padding-right: 8px;margin-top: -25px; position: relative;z-index: 2;" class="glyphicon glyphicon-eye-close"></i>
+									<i id="openeye" style="display:none;float: right;margin-left: -23px;padding-right: 8px; margin-top: -25px; position: relative;z-index: 2;" class="glyphicon glyphicon-eye-open"></i>
+								</div>
 
-					<div class="form-group">
-					<button  name="btn-login" class="btn btn-log btn-lg btn-success btn-block" id="btn-login" type="submit"><span class="fa fa-sign-in"></span> &nbsp;Check Report</button>
+								<div class="form-group">
+									<button  name="btn-login" class="btn btn-log btn-lg btn-success btn-block" id="btn-login" type="submit"><span class="fa fa-sign-in"></span> &nbsp;Check Report</button>
+								</div>
+							</form>
+						<?php } ?>
 					</div>
-				</form>
-				<?php } ?>
 				</div>
-		  </div>
+			</div>
 		</div>
-		</div>
-		</div>
-	</body>
+	</div>
+</body>
 </html>
 <script type="text/javascript" src="login/script.js"> </script>
--- login/script.js
+++ login/script.js
@@ -1,73 +1,72 @@
 
-	
-     /* validation */
-  $("#login-form").validate({
-      rules:
-   {
-   password: {
-   required: true,
-   },
-   user_email: {
-		minlength:5,
-		required: true,
+/* validation */
+$("#login-form").validate({
+	rules:
+	{
+		password: {
+			required: true,
 		},
-    },
-       messages:
-    {
-            password:{
-                      required: "Please enter your Password"
-                     },
-            user_email: {
-				required:"Please enter your Reference ID",
-				minlength: "Reference ID must be 5 or more characters"
+		user_email: {
+			minlength:5,
+			required: true,
+		},
+	},
+	messages:
+	{
+		password:{
+			required: "Please enter your Password"
+		},
+		user_email: {
+			required:"Please enter your Reference ID",
+			minlength: "Reference ID must be 5 or more characters"
+		}
+	},
+	submitHandler: submitForm
+});
+/* validation */
+
+/* login submit */
+function submitForm()
+{
+	var data = $("#login-form").serialize();
+
+	$.ajax({
+
+		type : 'POST',
+		url  : 'login/loginprocess.php',
+		data : data,
+		beforeSend: function()
+		{
+			$("#error").fadeOut();
+			$("#btn-login").html('<span class="fa fa-cog fa-spin"></span> &nbsp; Processing. Please wait ...');
+		},
+		success :  function(response)
+		{
+			if(response=='ok'){
+				$("#btn-login").html('<span class="fa fa-thumbs-o-up"></span> &nbsp; Signing In ...');
+				//setTimeout(' window.location.href = "home.php"; ',4000);
+
+				$("#login-area").load('login/logout.php');
+				$("#chatbox").load("chat/chat.php");
+
 			}
-       },
-    submitHandler: submitForm 
-       });  
-    /* validation */
-    
-    /* login submit */
-    function submitForm()
-    {  
-   var data = $("#login-form").serialize();
-    
-   $.ajax({
-    
-   type : 'POST',
-   url  : 'login/loginprocess.php',
-   data : data,
-   beforeSend: function()
-   { 
-    $("#error").fadeOut();
-    $("#btn-login").html('<span class="fa fa-cog fa-spin"></span> &nbsp; Processing. Please wait ...');
-   },
-   success :  function(response)
-      {      
-     if(response=='ok'){
-      $("#btn-login").html('<span class="fa fa-thumbs-o-up"></span> &nbsp; Signing In ...');
-      //setTimeout(' window.location.href = "home.php"; ',4000);
-	  
-			$("#login-area").load('login/logout.php');
-			$("#chatbox").load("chat/chat.php");
-	  
-     }
-     else{
-      $("#btn-login").fadeIn(1000, function(){      
-           $("#btn-login").html('<span class="glyphicon glyphicon-info-sign"></span> &nbsp; '+response+'!');
-         }).delay(3000).fadeIn(1000, function(){
-			 $("#btn-login").html('<span class="fa fa-sign-in"></span> &nbsp;Check Report');
-		 });
-		 
-		 
-		 
-		 
-     }
-     }
-   });
-    return false;
-  }
-    /* login submit */
-	
-	
-  
-    /* login submit */
+			else{
+				$("#btn-login").fadeIn(1000, function(){
+					$("#btn-login").html('<span class="glyphicon glyphicon-info-sign"></span> &nbsp; '+response+'!');
+				}).delay(3000).fadeIn(1000, function(){
+					$("#btn-login").html('<span class="fa fa-sign-in"></span> &nbsp;Check Report');
+				});
+
+
+
+
+			}
+		}
+	});
+	return false;
+}
+/* login submit */
+
+
+
+/* login submit */
